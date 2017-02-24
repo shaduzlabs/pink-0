2954f8dc5b701609b42df97902797a664ba9c120
Vincenzo Pacella <vincenzo.pacella@native-instruments.de>
2017-02-24 11:36:54 +0100

    update install script

--- install.sh
+++ install.sh
@@ -1,15 +1,35 @@
-        ##########    Copyright (C) 2016 Vincenzo Pacella
-        ##      ##    Distributed under MIT license, see file LICENSE
-        ##      ##    or <http://opensource.org/licenses/MIT>
-        ##      ##
+##########    Copyright (C) 2016 Vincenzo Pacella
+##      ##    Distributed under MIT license, see file LICENSE
+##      ##    or <http://opensource.org/licenses/MIT>
+##      ##
 ##########      ############################################################# shaduzlabs.com #######
 
-if [ ! -d "/home/pi/" ];
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+  if [ ! -d "/home/pi/" ];
+    then
+      platform='linux'
+    else
+      platform='linux-rpi'
+  fi
+elif [[ "$unamestr" == 'Darwin' ]];
+  then
+    which -s brew
+    if [[ $? != 0 ]] ;
+    then
+      echo -e "[ \033[1m\033[31mERROR\033[m ] Homebrew is required but it is not installed"
+      exit -2
+    fi
+    platform='osx'
+fi
+
+if [ $platform == "unknown" ];
   then
     echo ""
-    echo -e "[ \033[1m\033[31mERROR\033[m ] This install script can only be run on a Raspberry Pi!"
+    echo -e "[ \033[1m\033[31mERROR\033[m ] This install script is not compatible with the current platform."
     echo ""
-    exit -1
+   exit -1
 fi
 
 echo ""
@@ -38,17 +58,33 @@ echo -e "          \033[1m\033[96mSit down and relax, this will take some time..
 echo ""
 
 echo ""
+echo ""
+echo ""
 echo -e "[ \033[1m\033[96mpink\033[m ] Update submodules -----------------------------------------------------"
 git submodule update --init --recursive
 
 echo ""
+echo ""
+echo ""
 echo -e "[ \033[1m\033[96mpink\033[m ] Install dependencies --------------------------------------------------"
-sudo apt-get install -y --force-yes cmake wiringpi
+if [ $platform == "osx" ];
+  then
+    brew install cmake
+  else
+    sudo apt-get install -y --force-yes cmake wiringpi
+fi
 
 echo ""
+echo ""
+echo ""
 echo -e "[ \033[1m\033[96mpink\033[m ] Install RaspAP --------------------------------------------------------"
-wget -q https://git.io/vDr0i -O /tmp/raspap && bash /tmp/raspap
+if [ $platform == "linux-rpi" ];
+  then
+    wget -q https://git.io/vDr0i -O /tmp/raspap && bash /tmp/raspap
+fi
 
+echo ""
+echo ""
 echo ""
 echo -e "[ \033[1m\033[96mpink\033[m ] Build and install portaudio -------------------------------------------"
 cd modules/portaudio
@@ -58,22 +94,53 @@ sudo make install
 cd ../../
 
 echo ""
+echo ""
+echo ""
 echo -e "[ \033[1m\033[96mpink\033[m ] Build pink and install the binary and the daemon configuration file ---"
+rm -rf build
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+if [ $1 == "no-ui" ]
+  then
+    cmake -DCMAKE_BUILD_TYPE=Release -DUSE_PI_ZERO=OFF ..
+  else
+    echo -e "[ \033[1m\033[96mpink\033[m ] Raspberry Pi Zero shield support is enabled ---------------------------"
+    cmake -DCMAKE_BUILD_TYPE=Release -DUSE_PI_ZERO=ON ..
+fi
 make
-sudo make install
+if [ $platform == "linux-rpi" ];
+  then
+    sudo make install
+fi
 cd ..
 
 echo ""
+echo ""
+echo ""
 echo -e "[ \033[1m\033[96mpink\033[m ] Update device configuration -------------------------------------------"
-sudo cp --backup=numbered support/hostapd.conf /etc/hostapd/.
-sudo cp --backup=numbered support/boot/config.txt /boot/.
+if [ $platform == "linux-rpi" ];
+  then
+    sudo cp --backup=numbered support/hostapd.conf /etc/hostapd/.
+fi
 
+echo ""
+echo ""
 echo ""
 echo -e "[ \033[1m\033[96mpink\033[m ] Optimize system configuration -----------------------------------------"
-echo -n performance | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+if [ $platform == "linux-rpi" ];
+  then
+    echo -n performance | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+fi
 
 echo ""
-echo -e "[ \033[1m\033[96mpink\033[m ] Installation completed, please reboot now."
+echo ""
+echo ""
+if [ $platform == "linux-rpi" ];
+  then
+    echo -e "[ \033[1m\033[96mpink\033[m ] Installation completed, please reboot now."
+  else
+    echo -e "[ \033[1m\033[96mpink\033[m ] Build completed"
+fi
+echo ""
+echo ""
+echo ""
